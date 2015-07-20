f92ebbf959e12e371eec7af34901f49bfd9319cd
Edoardo <edo.basili@gmail.com>
2015-07-20 15:00:09 +0200

    optional userkey

--- eventid.js
+++ eventid.js
@@ -1,10 +1,10 @@
-function eventid(error,eventid,userkey,handleData){ 
+function eventid(error,eventid,handleData,userkey){ 
     if(error) {
     	console.err(error);
     	throw error;
     }
     var id = eventid;
-    var key = userkey;
+    var key = userkey || '9796e202068fb23423157831262a';
 	$.ajax({
 		url: 'https://api.meetup.com/2/event/'+id+'?key='+key+'&sign=true',
 		dataType: 'JSONP',
